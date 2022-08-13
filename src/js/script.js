5a8c961439b781dddef76b2c93087760e7998a61
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-13 15:23:27 -0300

    funcao modo dark mode semi pronto

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,18 @@
-const lamp = document.querySelector('.lamp')
-const css = document.querySelector('#css')
+const lamp = document.querySelector(".lamp");
+const css = document.querySelector("#css");
+
+const darkMode = () => {
+  const text = css.href.indexOf("src");
+  const info = css.href.substring(text);
+  const white = "src/style/main.css";
+  const dark = "src/style/main-dark.css";
+  if (info === dark) {
+    css.href = white;
+  } else {
+    css.href = dark;
+  }
+};
+
+lamp.addEventListener("click", darkMode);
+
+window.onload = () => {};
