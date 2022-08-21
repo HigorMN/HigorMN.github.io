af57f1e36e1f58e782a440bf45fc4c34694c24eb
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-21 15:54:04 -0300

    lista do header adicionada

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,21 @@ class Header extends React.Component {
     return (
       <header>
         <ul>
-          <li>Inicio</li>
+          <li id="inicio">
+            <a href="inicio">Início</a>
+          </li>
+          <li>
+            <a href="sobre">Sobre</a>
+          </li>
+          <li>
+            <a href="projetos">Projetos</a>
+          </li>
+          <li>
+            <a href="habilidades">Habilidades</a>
+          </li>
+          <li>
+            <a href="contato">Contato</a>
+          </li>
         </ul>
       </header>
     );
