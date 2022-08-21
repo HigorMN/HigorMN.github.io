aa93e0f534a0fbf97e50cbe14343322142ea13b5
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-21 15:46:38 -0300

    Exportando o Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <ul>
+          <li>Inicio</li>
+        </ul>
+      </header>
+    );
+  }
+}
+
+export default Header;
