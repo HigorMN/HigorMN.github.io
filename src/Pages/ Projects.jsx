38b0e7bb0884cc491a4956db4286134788f34f30
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-09-23 15:22:53 -0300

    ajuste

--- src/Pages/ Projects.jsx
+++ src/Pages/ Projects.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Projects extends Component {
   render() {
     return (
-      <div id="projects-container">
+      <div id="projects-container" className="center">
         <h1>Pagina de projetos em manutenção.</h1>
       </div>
     );
