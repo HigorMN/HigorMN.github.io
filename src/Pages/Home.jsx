10c50288815296018f443e23c0d7b2ef3e3c4659
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-09-23 13:48:32 -0300

    colocando o componente de contato na home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import linkedin from '../images/linkedin.png';
-import gmail from '../images/gmail.png';
-import github from '../images/github.png';
 import Arrow from '../components/Arrow';
+import ContactLinks from '../components/ ContactLinks';
 
 export default class Home extends Component {
   render() {
@@ -12,25 +10,7 @@ export default class Home extends Component {
           <h3>Olá, eu sou</h3>
           <h1>Higor Maranhão</h1>
           <h3>Fullstack Develover</h3>
-          <nav className="contact-nav-container">
-            <a
-              href="https://github.com/HigorMN"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={github} alt="GitHub do Higor" />
-            </a>
-            <a href="mailto:higor.maranhao2000@gmail.com">
-              <img src={gmail} alt="Email do Higor" />
-            </a>
-            <a
-              href="https://www.linkedin.com/in/higor-maranhao/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={linkedin} alt="Linkedin do Higor" />
-            </a>
-          </nav>
+          <ContactLinks />
         </div>
         <Arrow to="#about" />
         <div className="center mt" id="about">
