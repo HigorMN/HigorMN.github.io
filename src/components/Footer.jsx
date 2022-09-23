a9be614acc0894b334d62a50acbb741d0d688d5d
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-09-23 13:39:49 -0300

    Ajustando nomes das logo png no footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import linkedin from '../images/linkedin.png';
-import gmail from '../images/gmail.png';
-import github from '../images/github.png';
+import linkedinLogoPNG from '../images/linkedin.png';
+import gmailLogoPNG from '../images/gmail.png';
+import gitHubLogoPNG from '../images/github.png';
 
 export default class Footer extends Component {
   render() {
@@ -11,17 +11,17 @@ export default class Footer extends Component {
         <p>Estamos a um click de distância!!</p>
         <nav className="contact-nav-container">
           <a href="https://github.com/HigorMN" target="_blank" rel="noopener noreferrer">
-            <img src={github} alt="GitHub do Higor" />
+            <img src={gitHubLogoPNG} alt="GitHub do Higor" />
           </a>
           <a href="mailto:higor.maranhao2000@gmail.com">
-            <img src={gmail} alt="Email do Higor" />
+            <img src={gmailLogoPNG} alt="Email do Higor" />
           </a>
           <a
             href="https://www.linkedin.com/in/higor-maranhao/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img src={linkedin} alt="Linkedin do Higor" />
+            <img src={linkedinLogoPNG} alt="Linkedin do Higor" />
           </a>
         </nav>
       </footer>
