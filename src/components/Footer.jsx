d2f87effbe7ac32c17cd57d689fcfedcb4cd2dc0
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-09-23 13:47:00 -0300

    criando componente de contatos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import linkedinLogoPNG from '../images/linkedin.png';
-import gmailLogoPNG from '../images/gmail.png';
-import gitHubLogoPNG from '../images/github.png';
+import ContactLinks from './ ContactLinks';
 
 export default class Footer extends Component {
   render() {
@@ -9,21 +7,7 @@ export default class Footer extends Component {
       <footer id="footer">
         <h2 className="h2">Contato</h2>
         <p>Estamos a um click de distância!!</p>
-        <nav className="contact-nav-container">
-          <a href="https://github.com/HigorMN" target="_blank" rel="noopener noreferrer">
-            <img src={gitHubLogoPNG} alt="GitHub do Higor" />
-          </a>
-          <a href="mailto:higor.maranhao2000@gmail.com">
-            <img src={gmailLogoPNG} alt="Email do Higor" />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/higor-maranhao/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={linkedinLogoPNG} alt="Linkedin do Higor" />
-          </a>
-        </nav>
+        <ContactLinks />
       </footer>
     );
   }
