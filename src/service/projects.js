126448fe77c3cd030677d2603e3903f70b17c9e4
HigorMN <higor.maranhao2000@gmail.com>
2022-11-12 20:48:11 -0300

    build: projeto lessons-learned adicionado no data

---
+++ src/service/projects.js
@@ -0,0 +1,10 @@
+const data = [{
+  name: 'Lessons Learned',
+  module: 'trybe - fundamentos',
+  topics: ['HTML', 'CSS'],
+  image: '../service/images/lessons-learned',
+  linkWeb: 'https://higormn.github.io/lessons-learned/',
+  linkRepository: 'https://github.com/HigorMN/lessons-learned',
+}];
+
+export default data;
