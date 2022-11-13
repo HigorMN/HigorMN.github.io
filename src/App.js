42835c4ea56101997374e94886d66524a67b87d1
HigorMN <higor.maranhao2000@gmail.com>
2022-11-12 21:37:41 -0300

    fix: importacao

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './Pages/Home';
-import './css/Main.css'
-import './css/Header.css'
+import './css/Header.css';
+import './css/Main.css';
 import Footer from './components/Footer';
 import About from './Pages/About';
 import Projects from './Pages/Projects';
-
 
 export default class App extends Component {
   render() {
@@ -27,7 +26,6 @@ export default class App extends Component {
         </Switch>
         <Footer />
       </>
-    )
+    );
   }
 }
-
