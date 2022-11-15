68ffc867833348af72cf4b2b0804b190e0e6d73d
HigorMN <higor.maranhao2000@gmail.com>
2022-11-15 11:20:21 -0300

    feat: adicionando img de HTML e CSS

--- src/service/projects.js
+++ src/service/projects.js
@@ -1,9 +1,12 @@
 import lessonsLearned from './images/lessons-learned.png';
 
+const HTML = 'https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg';
+const CSS = 'https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg';
+
 const data = [{
   name: 'Lessons Learned',
   module: 'trybe - fundamentos',
-  topics: ['HTML', 'CSS'],
+  topics: [HTML, CSS],
   image: lessonsLearned,
   linkWeb: 'https://higormn.github.io/lessons-learned/',
   linkRepository: 'https://github.com/HigorMN/lessons-learned',
