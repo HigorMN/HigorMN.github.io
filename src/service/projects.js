d1fe669c522c4a867f8e3064e965088b9f5d198f
HigorMN <higor.maranhao2000@gmail.com>
2022-11-15 12:55:04 -0300

    chore: adicionando projeto

--- src/service/projects.js
+++ src/service/projects.js
@@ -1,13 +1,22 @@
 import lessonsLearned from './images/lessons-learned.png';
+import PlaygroundFunctions from './images/Playground-Functions.png';
 
 const HTML = 'https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg';
 const CSS = 'https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg';
+const JS = 'https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg';
 
 const data = [{
   name: 'Lessons Learned',
   module: 'trybe - fundamentos',
   topics: [HTML, CSS],
   image: lessonsLearned,
+  linkWeb: 'https://higormn.github.io/lessons-learned/',
+  linkRepository: 'https://github.com/HigorMN/lessons-learned',
+}, {
+  name: 'Playground Functions',
+  module: 'trybe - fundamentos',
+  topics: [JS],
+  image: PlaygroundFunctions,
   linkWeb: 'https://higormn.github.io/lessons-learned/',
   linkRepository: 'https://github.com/HigorMN/lessons-learned',
 }];
