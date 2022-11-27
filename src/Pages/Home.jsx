53fbdc898bdfda04568467746320d3371369d855
HigorMN <higor.maranhao2000@gmail.com>
2022-11-27 13:42:43 -0300

    feat: Aba habilidades concluido

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -18,6 +18,41 @@ export default class Home extends Component {
         <About />
         <Arrow to="#projects" />
         <Projects />
+        <Arrow to="#skills" />
+        <div id="skills">
+          <div className="center mt" id="about">
+            <h2 className="h2">Habilidades</h2>
+          </div>
+          <div className="Hab__Container__List">
+            <ul>
+              <p>Desenvolvedor Front-end</p>
+              <li>React</li>
+              <li>JavaScript</li>
+              <li>Redux</li>
+              <li>Hooks</li>
+              <li>Jest</li>
+              <li>RTL</li>
+              <li>TDD</li>
+              <li>Git</li>
+              <li>GitHub</li>
+            </ul>
+            <ul>
+              <p>Liderança</p>
+              <li>Ágil</li>
+              <li>SCRUM</li>
+              <li>Kanban</li>
+              <li>Code Review</li>
+              <li>Pair programming</li>
+            </ul>
+            <ul>
+              <p>Características</p>
+              <li>Empatia</li>
+              <li>Adaptabilidade</li>
+              <li>Criatividade</li>
+              <li>Paciência</li>
+            </ul>
+          </div>
+        </div>
         <Arrow to="#footer" />
       </main>
     );
