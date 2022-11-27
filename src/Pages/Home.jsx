75cf07ba15b2ae9c55c8110e7bee3eb11beb2fe6
HigorMN <higor.maranhao2000@gmail.com>
2022-11-27 13:46:33 -0300

    feat: criando component Skills

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Arrow from '../components/Arrow';
 import ContactLinks from '../components/ContactLinks';
+import Skills from '../components/Skills';
 import About from './About';
 import Projects from './Projects';
 
@@ -19,40 +20,7 @@ export default class Home extends Component {
         <Arrow to="#projects" />
         <Projects />
         <Arrow to="#skills" />
-        <div id="skills">
-          <div className="center mt" id="about">
-            <h2 className="h2">Habilidades</h2>
-          </div>
-          <div className="Hab__Container__List">
-            <ul>
-              <p>Desenvolvedor Front-end</p>
-              <li>React</li>
-              <li>JavaScript</li>
-              <li>Redux</li>
-              <li>Hooks</li>
-              <li>Jest</li>
-              <li>RTL</li>
-              <li>TDD</li>
-              <li>Git</li>
-              <li>GitHub</li>
-            </ul>
-            <ul>
-              <p>Liderança</p>
-              <li>Ágil</li>
-              <li>SCRUM</li>
-              <li>Kanban</li>
-              <li>Code Review</li>
-              <li>Pair programming</li>
-            </ul>
-            <ul>
-              <p>Características</p>
-              <li>Empatia</li>
-              <li>Adaptabilidade</li>
-              <li>Criatividade</li>
-              <li>Paciência</li>
-            </ul>
-          </div>
-        </div>
+        <Skills />
         <Arrow to="#footer" />
       </main>
     );
