94e6348c79d59e303b0ffb34ff17bc8dc70db927
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-04-12 17:33:45 -0300

    style: logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import Socials from '../Socials';
 import Menu from '../Menu';
 
 import './Header.css';
 import logo from '../../images/logo.png';
+import animationContext from '../../context/animationContext';
 
 export default function Header() {
+  const { handleNavigate } = useContext(animationContext);
   return (
     <header className="header">
-      <img className="logo" src={logo} alt="logo" />
+      <nav className="menu">
+        <button
+          type="button"
+          className="none"
+          onClick={handleNavigate}
+          name="/"
+        >
+            <img className="logo" src={logo} alt="logo" />
+        </button>
+      </nav>
       <Socials />
       <Menu />
     </header>
